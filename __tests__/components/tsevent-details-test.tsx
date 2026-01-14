94218a4f2893d711ff228fbf36538efe6f4e933d
Jon Turner <jon.e.turner@outlook.com>
2026-01-13 23:33:57 -0800

    `TsEventDetails` tests
    - Added more tests
    - Fixed a bug they exposed

--- __tests__/components/tsevent-details-test.tsx
+++ __tests__/components/tsevent-details-test.tsx
@@ -7,11 +7,10 @@ import {
 } from '@testing-library/react-native';
 
 describe('<TsEventDetails />', () => {
-  const handleDetailsChange = (
-    id: number,
-    prop?: string,
-    newValue?: string
-  ) => {};
+  const handleDetailsChange = jest.fn();
+
+  beforeEach(() => jest.clearAllMocks());
+
   it('Renders all the default elements', async () => {
     const { getByPlaceholderText, getByLabelText } = render(
       <TsEventDetails
@@ -68,6 +67,137 @@ describe('<TsEventDetails />', () => {
       getByLabelText('detail-text');
       getByLabelText('done');
       getByLabelText('delete'); // changed from 'close'
+    });
+  });
+
+  it('Updates properly when the text changes back to the original', async () => {
+    const { getByDisplayValue, getByLabelText } = render(
+      <TsEventDetails
+        tsEventId={1}
+        handleDetailsChange={handleDetailsChange}
+      />,
+      {}
+    );
+
+    await act(async () =>
+      fireEventAsync.changeText(
+        getByLabelText('detail-text'),
+        'Romani ite domum'
+      )
+    );
+
+    await act(async () =>
+      fireEventAsync.changeText(getByLabelText('detail-text'), '')
+    );
+
+    await waitFor(() => {
+      getByDisplayValue('');
+      getByLabelText('detail-text');
+      getByLabelText('done');
+      getByLabelText('close');
+    });
+  });
+
+  it('Calls the updater function with correct arguments when text has changed and done is pressed', async () => {
+    const { getByDisplayValue, getByLabelText } = render(
+      <TsEventDetails
+        tsEventId={1}
+        handleDetailsChange={handleDetailsChange}
+      />,
+      {}
+    );
+
+    await act(async () =>
+      fireEventAsync.changeText(
+        getByLabelText('detail-text'),
+        'Romani ite domum'
+      )
+    );
+
+    await act(async () => fireEventAsync.press(getByLabelText('done')));
+
+    await waitFor(() => {
+      getByDisplayValue('Romani ite domum');
+      getByLabelText('detail-text');
+      getByLabelText('done');
+      getByLabelText('close');
+    });
+
+    expect(handleDetailsChange).toHaveBeenCalledTimes(1);
+    expect(handleDetailsChange).toHaveBeenCalledWith(
+      1,
+      'details',
+      'Romani ite domum'
+    );
+  });
+
+  it('Calls the updater function with only the id when text has not changed and done is pressed', async () => {
+    const { getByDisplayValue, getByLabelText } = render(
+      <TsEventDetails
+        tsEventId={1}
+        handleDetailsChange={handleDetailsChange}
+      />,
+      {}
+    );
+
+    await act(async () => fireEventAsync.press(getByLabelText('done')));
+
+    await waitFor(() => {
+      getByDisplayValue('');
+      getByLabelText('detail-text');
+      getByLabelText('done');
+      getByLabelText('close');
+    });
+
+    expect(handleDetailsChange).toHaveBeenCalledTimes(1);
+    expect(handleDetailsChange).toHaveBeenCalledWith(1, undefined, undefined);
+  });
+
+  it('Calls the updater function with only the id when text has not changed and close is pressed', async () => {
+    const { getByDisplayValue, getByLabelText } = render(
+      <TsEventDetails
+        tsEventId={1}
+        handleDetailsChange={handleDetailsChange}
+      />,
+      {}
+    );
+
+    await act(async () => fireEventAsync.press(getByLabelText('close')));
+
+    await waitFor(() => {
+      getByDisplayValue('');
+      getByLabelText('detail-text');
+      getByLabelText('done');
+      getByLabelText('close');
+    });
+
+    expect(handleDetailsChange).toHaveBeenCalledTimes(1);
+    expect(handleDetailsChange).toHaveBeenCalledWith(1, undefined, undefined);
+  });
+
+  it('Calls the updater function with just the id when text has changed and cancel is pressed', async () => {
+    const { getByDisplayValue, getByLabelText } = render(
+      <TsEventDetails
+        tsEventId={1}
+        handleDetailsChange={handleDetailsChange}
+      />,
+      {}
+    );
+
+    await act(async () =>
+      fireEventAsync.changeText(
+        getByLabelText('detail-text'),
+        'Romani ite domum'
+      )
+    );
+
+    await act(async () => fireEventAsync.press(getByLabelText('delete')));
+
+    await waitFor(() => {
+      getByDisplayValue('');
+      getByLabelText('detail-text');
+      getByLabelText('done');
+      getByLabelText('close');
     });
   });
 });
