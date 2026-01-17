dd029eaf6d085fab5040faf79b7c1dcdb1156232
Jon Turner <jon.e.turner@outlook.com>
2026-01-16 21:06:58 -0800

    Removing dev lense

--- types/lense-check.ts
+++
@@ -1,8 +0,0 @@
-export type LenseCheck = {
-  check: boolean | (() => boolean);
-  label?: string;
-};
-
-export const LenseTests: LenseCheck[] = [
-  { check: () => 1 === 1, label: 'Index: 1 === 1' },
-];
