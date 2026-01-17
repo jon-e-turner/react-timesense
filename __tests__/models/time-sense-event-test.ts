94e71ddd8375173b9b8de9612a029fa5935a5063
Jon Turner <158862421+jon-e-turner@users.noreply.github.com>
2026-01-16 21:27:39 -0800

    Feature/model tests (#49)
    * Fix eslint errors
    
    * Added a second test for `TimeSenseEvent`
    
    - Tests default and fully configured branches

--- __tests__/models/time-sense-event-test.ts
+++ __tests__/models/time-sense-event-test.ts
@@ -1,15 +1,43 @@
 import { TimeSenseEvent } from '@/models/time-sense-event';
 import { DEFAULT_EVENT_GLYPH } from '@/types/icons';
+import { UTCDate } from '@date-fns/utc';
 
 describe('TimeSenseEvent class', () => {
-  test('when given just a name, returns the default object', () => {
-    const tsEvent = new TimeSenseEvent({ name: 'test-event' });
+  it('when given just a name, returns the default object', () => {
+    const tsEvent = new TimeSenseEvent({ name: 'test event' });
 
-    expect(tsEvent.details).not.toBeDefined();
+    expect(tsEvent.details).toBeUndefined();
     expect(tsEvent.icon).toBe(DEFAULT_EVENT_GLYPH);
     expect(tsEvent.triggerHistory).toBeDefined();
-    expect(tsEvent.triggerHistory.length).toBe(0);
-    expect(tsEvent.name).toBe('test-event');
+    expect(tsEvent.triggerHistory).toHaveLength(0);
+    expect(tsEvent.name).toBe('test event');
     expect(tsEvent.id).toBe(0);
+  });
+
+  // Simulates loading an event from the database.
+  it('when given optional props, respects them', () => {
+    const tsEvent = new TimeSenseEvent({
+      id: 4,
+      name: 'test event',
+      icon: 'gamepad',
+      details: 'A long time ago...',
+      triggerHistory: [
+        {
+          timestamp: new UTCDate('2026-01-10T13:00:00Z'),
+          tags: ['user'],
+        },
+      ],
+    });
+
+    expect(tsEvent.details).toBe('A long time ago...');
+    expect(tsEvent.icon).toBe('gamepad');
+    expect(tsEvent.triggerHistory).toBeDefined();
+    expect(tsEvent.triggerHistory).toHaveLength(1);
+    expect(tsEvent.triggerHistory[0].timestamp).toStrictEqual(
+      new UTCDate('2026-01-10T13:00:00Z')
+    );
+    expect(tsEvent.triggerHistory[0].tags).toContain('user');
+    expect(tsEvent.name).toBe('test event');
+    expect(tsEvent.id).toBe(4);
   });
 });
