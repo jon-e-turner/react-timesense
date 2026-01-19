d1e2c25d428ebb464724b884d708341f5909ad58
Jon Turner <jon.e.turner@outlook.com>
2026-01-18 19:17:21 -0800

    Add eslint plugin for testing library

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 const jest = require('eslint-plugin-jest');
+const testingLibrary = require('eslint-plugin-testing-library');
 
 module.exports = defineConfig([
   ...expoConfig,
@@ -9,6 +10,7 @@ module.exports = defineConfig([
     files: ['__tests__/**'],
     ...jest.configs['flat/recommended'],
     ...jest.configs['flat/style'],
+    ...testingLibrary.configs['flat/react'],
   },
   {
     ignores: ['dist/*'],
